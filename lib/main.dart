9c943479c197e20b7274a03624d4de85fe3f902a
Sharath Kumar <1999sharathkumar7@gmail.com>
2023-10-23 17:52:40 +0530

    first commit UI completed

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+import 'screens/mobile_layout_screen.dart';
+import 'screens/web_layout_screen.dart';
+import 'utils/responsive_layout.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ChatterBox',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: backgroundColor,
+        // textTheme: GoogleFonts.quicksandTextTheme(),
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileLayoutScreen(),
+        webScreenLayout: WebLayoutScreen(),
+      ),
+    );
+  }
+}
---
+++ lib/screens/mobile_layout_screen.dart
@@ -0,0 +1,69 @@
+import 'package:chatterbox/colors.dart';
+import 'package:chatterbox/widgets/contacts_list.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MobileLayoutScreen extends StatelessWidget {
+  const MobileLayoutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          centerTitle: false,
+          title:  Text(
+            'ChatterBox',
+            style: GoogleFonts.quicksand(
+              fontSize: 20,
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search, color: Colors.grey),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert, color: Colors.grey),
+              onPressed: () {},
+            ),
+          ],
+          bottom:  TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: GoogleFonts.quicksand(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs:const [
+              Tab(
+                text: 'Chatter',
+              ),
+              Tab(
+                text: 'Stories',
+              ),
+              Tab(
+                text: 'Connect',
+              ),
+            ],
+          ),
+        ),
+        body: const ContactsList(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: tabColor,
+          child: const Icon(
+            Icons.message_rounded,
+            color: Colors.white24,
+          ),
+        ),
+      ),
+    );
+  }
+}
