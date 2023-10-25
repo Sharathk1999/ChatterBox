b5269f97cd380f3d2e65fae6ffc4f7f2b29bbb52
Sharath Kumar <1999sharathkumar7@gmail.com>
2023-10-25 11:57:10 +0530

    Changed bg color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,12 +1,12 @@
 import 'dart:ui';
 
-const backgroundColor = Color.fromRGBO(74, 82, 85, 1);
+const backgroundColor = Color.fromRGBO(83, 82, 82, 1);
 const textColor = Color.fromRGBO(241, 241, 242, 1);
 const appBarColor = Color.fromRGBO(52, 53, 53, 1);
 const webAppBarColor = Color.fromRGBO(42,47,50,1);
 const messageColor = Color.fromRGBO(104, 106, 106, 1);
 const senderMessageColor = Color.fromRGBO(37, 45, 49, 1);
-const tabColor = Color.fromRGBO(132, 134, 133, 1);
+const tabColor = Color.fromRGBO(50, 51, 50, 1);
 const searchBarColor = Color.fromRGBO(50, 55, 57, 1);
 const dividerColor = Color.fromRGBO(37, 45, 50, 1);
 const chatBarMessage = Color.fromRGBO(30, 36, 40, 1);
