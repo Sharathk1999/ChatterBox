5044ef80f45cce4863c3eb8bf65ddc18f28304ed
Sharath Kumar <1999sharathkumar7@gmail.com>
2023-11-11 03:32:18 +0530

    Added audio play feature

--- lib/features/chat/widgets/display_image_gif.dart
+++ lib/features/chat/widgets/display_image_gif.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chatterbox/colors.dart';
 import 'package:chatterbox/common/enums/message_enum.dart';
@@ -11,22 +12,41 @@ class DisplayImageGIF extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isPlaying = false;
+    final AudioPlayer audioPlayer = AudioPlayer();
     return type == MessageEnum.text
         ? Text(
             message,
             style: const TextStyle(fontSize: 16, color: whiteColor),
           )
         : type == MessageEnum.audio
-            ? IconButton(
-              constraints: const BoxConstraints(
-                minWidth: 200,
-          
-              ),
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.play_circle_fill_rounded,
+            ? StatefulBuilder(
+              builder: (context, setState) {
+                
+              return IconButton(
+                constraints: const BoxConstraints(
+                  minWidth: 200,
+                      
                 ),
-              )
+                  onPressed: () async{
+                    if (isPlaying) {
+                      await audioPlayer.pause();
+                      setState(() {
+                        isPlaying = false;
+                      },);
+                    }else{
+                      await audioPlayer.play(UrlSource(message));
+                      setState(() {
+                        isPlaying=true;
+                      },);
+                    }
+                  },
+                  icon:  Icon(
+                   isPlaying ? Icons.pause_circle: Icons.play_circle_fill_rounded,
+                  ),
+                );
+              }
+            )
             : type == MessageEnum.video
                 ? VideoPlayerWidget(
                     videoUrl: message,
